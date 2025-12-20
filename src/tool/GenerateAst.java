97edb5b629ec9a5436c6f654f0f34a790262e1ce
aradganir <arad@ganir.net>
2025-12-19 23:35:06 -0800

    Created the tree parser/printer

--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -16,9 +16,18 @@ public class GenerateAst {
         defineAst(outputDir, "Expr", Arrays.asList(
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
-                "Literal  : Object Value",
+                "Literal  : Object value",
                 "Unary    : Token operator, Expr right"
         ));
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println(" interface Visitor<R> {");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println("    }");
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
@@ -31,12 +40,16 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         //AST Classes
         for(String type: types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+        writer.println();
+        writer.println(" abstract <R> R accept(Visitor<R> visitor);");
         writer.println("}");
         writer.println();
         writer.close();
@@ -62,6 +75,15 @@ public class GenerateAst {
             writer.println("    final " + field + ";");
         }
 
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("        return visitor.visit" + className + baseName + "(this);");
         writer.println("    }");
+
+
+        writer.println("    }");
+
+
     }
 }
