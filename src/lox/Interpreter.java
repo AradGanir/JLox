73a9bd7f593dd9ab23c5bc25bd668867922c6eef
aradganir <arad@ganir.net>
2025-12-20 19:05:20 -0800

    Implemented interpretation

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -83,6 +83,20 @@ public class Interpreter implements Expr.Visitor<Object> {
         if(a == null) return false;
         return a.equals(b);
     }
+
+    private String stringify(Object object) {
+        if(object ==null) return "nil";
+
+        if(object instanceof Double) {
+            String text = object.toString();
+            if(text.endsWith(".0")) {
+                text = text.substring(0, text.length()-2);
+            }
+            return text;
+        }
+        return object.toString();
+    }
+
     //unary
     private void checkNumberOperand(Token operator, Object operand) {
         if (operand instanceof Double) return;
@@ -96,5 +110,17 @@ public class Interpreter implements Expr.Visitor<Object> {
 
     private boolean checkZero(Object operand2){
         return operand2 instanceof Double;
+    }
+
+
+
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch(RuntimeError error) {
+            Lox.runtimeError(error);
+        }
     }
 }
