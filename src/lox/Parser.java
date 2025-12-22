d77b1e2de486ca83f8ea19ba53248c39a6ffdaa2
Arad Ganir <67066916+AradGanir@users.noreply.github.com>
2025-12-22 13:57:19 -0800

    Controlflow (#8)
    * Added if/else
    
    * Logical OR and AND operators
    
    * While loops
    
    * Added while loops

--- src/lox/Parser.java
+++ src/lox/Parser.java
@@ -1,6 +1,7 @@
 package lox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import static lox.TokenType.*;
 
@@ -78,12 +79,88 @@ public class Parser {
         return tokens.get(current-1);
     }
 
+    private Stmt statement() {
+        if(match(PRINT)) return printStatement();
+        if(match(LEFT_BRACE)) return new Stmt.Block(Block());
+        if(match(IF)) return ifStatement();
+        if(match(WHILE)) return whileStatement();
+        if(match(FOR)) return forStatement();
+
+        return expressionStatement();
+    }
+    private Stmt forStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+        Stmt initializer;
+        if (match(SEMICOLON)) {
+            initializer = null;
+        } else if (match(VAR)) {
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement();
+        }
+
+        Expr condition = null;
+        if (!check(SEMICOLON)) {
+            condition = expression();
+        }
+        consume(SEMICOLON, "Expect ';' after 'for'.");
+
+        Expr increment = null;
+        if (!check(RIGHT_PAREN)) {
+            increment = expression();
+        }
+        consume(RIGHT_PAREN, "Expect ')' after 'for' clauses.");
+
+        Stmt body = statement();
+
+        if (increment != null) {
+            body = new Stmt.Block(
+                    Arrays.asList(
+                            body,
+                            new Stmt.Expression(increment)));
+        }
+
+        if(condition == null) condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+
+        return body;
+    }
+
+    private Stmt whileStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'while'");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after 'while'");
+        Stmt body = statement();
+
+        return new Stmt.While(condition, body);
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'if'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after 'if'.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+
+        if(match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
+    }
+
     private Expr expression() {
         return assignment();
     }
 
     private Expr assignment(){
-        Expr expr = equality();
+        Expr expr = or();
         if(match(EQUAL)) {
             Token equals = previous();
             Expr value = assignment();
@@ -94,10 +171,31 @@ public class Parser {
             }
             error(equals, "Invalid assignment target");
         }
-
         return expr;
     }
 
+    private Expr or() {
+        Expr expr = and();
+
+        while(match(OR)) {
+            Token operator = previous();
+            Expr right = and();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr and() {
+        Expr expr = equality();
+
+        while(match(AND)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+
+        return expr;
+    }
     private Expr equality() {
         Expr expr = comparison();
 
@@ -181,12 +279,7 @@ public class Parser {
         return statements;
     }
 
-    private Stmt statement() {
-        if(match(PRINT)) return PrintStatement();
-        if(match(LEFT_BRACE)) return new Stmt.Block(Block());
 
-        return expressionStatement();
-    }
 
     private List<Stmt> Block() {
         List<Stmt> statements = new ArrayList<>();
@@ -197,7 +290,7 @@ public class Parser {
         return statements;
     }
 
-    private Stmt PrintStatement() {
+    private Stmt printStatement() {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
         return new Stmt.Print(value);
