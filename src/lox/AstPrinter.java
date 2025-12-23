0c337c1c9684481367c053bfe6f1ce0eb54dc3a1
aradganir <arad@ganir.net>
2025-12-22 17:04:54 -0800

    Added rpnPrintr as a printer to the printer class

--- src/lox/AstPrinter.java
+++ src/lox/AstPrinter.java
@@ -47,3 +47,37 @@ public class AstPrinter implements Expr.Visitor<String> {
 
     }
 }
+
+class RpnPrinter implements Expr.Visitor<String> {
+    @Override public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + " " + expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    @Override public String visitLogicalExpr(Expr.Logical expr) {
+        return null;
+    }
+
+    @Override public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override public String visitLiteralExpr(Expr.Literal expr) {
+        return expr.value.toString();
+    }
+
+    @Override public String visitVariableExpr(Expr.Variable expr) {
+        return null;
+    }
+    @Override public String visitAssignExpr(Expr.Assign expr) {
+        return null;
+    }
+
+    @Override public String visitUnaryExpr(Expr.Unary expr) {
+        String operator =  expr.operator.lexeme;
+        if (expr.operator.type == TokenType.MINUS){
+            operator = "~";
+        }
+
+        return expr.right.accept(this) + " " + operator;
+    }
+}
